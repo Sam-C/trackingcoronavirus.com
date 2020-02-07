d14714a3618f7c591b4fc276e28af6b581cbe570
Samuel C <chansaukin@gmail.com>
2020-02-07 23:02:03 +0800

    Modify plan for server.js
    - use simpler intermediate data structures for ease of processing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const request = require("request");
+const rp = require("request-promise");
 const fs = require("fs");
-const papa = require("papaparse")
+const papa = require("papaparse");
 
 ////////////////////  data sources (CSV)  ////////////////////
 const dataSources = [{
@@ -14,46 +14,6 @@ const dataSources = [{
     url: "https://docs.google.com/spreadsheets/d/1UF2pSkFTURko2OvfHWWlFpDFAr1UxCBA4JLwlSP6KFo/export?format=csv&id=1UF2pSkFTURko2OvfHWWlFpDFAr1UxCBA4JLwlSP6KFo&gid=1056055583"
 }];
 
-
-////////////////////  intermediate data structures  ////////////////////
-
-/* right after extractData()
-
-let data = {
-    dates: ["1/21/2020 10:00 PM", ...],
-    regions: [{
-        province: "Anhui",
-        country: "Mainland China",
-        case_counts: [...]
-    }, {
-        province: "",
-        country: "Thailand",
-        case_counts: [...]
-    }, {
-        ...
-    }]
-};
-*/
-
-/* right after calculateCountryAndWorldTotal()
-
-let data = {
-    dates: ["Jan 21"],
-    regions: [{
-        name: "Anhui",
-        case_counts: [...]
-    }, {
-        name: "Mainland China", ...
-    }, {
-        name: "China", ...
-    }, {
-        name: "Thailand", ...
-    }, {
-        ...
-    }]
-};
-*/
-
 ////////////////////  overall flow (pseudocode) ////////////////////
 
 /*
@@ -61,85 +21,74 @@ for each source:
     download csv from the web
     save a copy to drive
     convert to 2D-array
-    extract dates[], province, country, case counts[]
     get country and world totals
     get date totals (and format date string)
+    extract dates[], province, country, case counts[]
     merge into data.js
 */
 
-////////////////////  top-level functions  ////////////////////
-
+////////////////////  data structures ////////////////////
+{
 /*
-let data = {};
-for (let i = 0; i < dataSources.length; i++) {
-    let csvString = fetchCsv(url);
-    saveToDisk(fileName, csvString);
-    let array2D = convertTo2DArray(csvString);
-    let caseData = extractData(array2D);
-    caseData = calculateCountryAndWorldTotal(caseData);
-    caseData = calculateDateTotal(caseData);
-    merge(data, caseData);
+rows = [
+    [Anhui,Mainland China,1/3/2020,31.82571,117.2264,,1,9,15,...],
+    [Beijing,Mainland China,1/3/2020,40.18238,116.4142,10,14,22,...],
+    ...
+    [,South Korea,1/21/2020,37.5665,126.978,,1,1,1,...],
+    [Taiwan,Taiwan,1/21/2020,23.6978,120.9605,1,1,1,...],
+    ...
+]
+
+provinces = [
+    {Anhui: [...]}, 
+    {Beijing: [...]},
+    // not {Taiwan: [...]}, because province == country
+]
+
+countries = [
+    {"Mainland China": [...]},
+    {"South Korea": [...]},
+    {Taiwan: [...]}
+]
+
+world = [
+    {"World": [...]}
+]
+
+regions = [world join countries join provinces]
+dates = [...]
+
+conbinded.splice(startingIndex, deleteCount)
+
+data = {
+    date: [5,6,7],
+    count: {
+        "Taiwan": {
+            "confirmed": [1,2,3]
+        }
+    }
 }
 */
+}
+////////////////////  top-level functions  ////////////////////
 
-////////////////////  ? ////////////////////
+let data = {};
 
-// let data = {};
-// for (let i = 0; i < dataSources.length; i++) {
-//     let csvString = await fetchCsv(source.url);
-//     fs.writeFileSync(`${source.caseType}.csv`, csvString);
+for (let i = 0; i < dataSources.length; i++) {
+    let url = dataSources[i].url;
+    let fileName = dataSources[i].caseType + ".csv";
 
-// }
+    let csvString = await rp(url);          // download csv from the web
+    fs.writeFileSync(fileName, csvString);  // save a copy to disk
+    let rows = papa.parse(csvString);       // convert to 2D-array
 
+    let dates = rows[0].slice(5);            // extract dates[]
+    rows = rows.slice(1);                    // remove the header row
+    let counts = calculateRegionTotal(rows); // extract counts[]
 
-// dataSources.forEach(source => {
-//     let csvString = await fetchCsv(source.url);
-//     fs.writeFileSync(`${source.caseType}.csv`, csvString);
+    [trimmedDates, trimmedCount] = trim(dates, counts);   // keep 1 column for each date
+    merge(data, trimmedDates, trimmedCount);              // merge into the data object
 
-// })
+}
 
-
-////////////////////  download CSV  ////////////////////
-// function fetchCsv(url, fileName) {
-//     request(url, (err, res, body) => {
-//         if (err) { return console.log(err); }
-//         return body;
-//     })
-// }
-
-// fetchCsv(dataSources[0].url);
-
-// dataSources.map(function(source) {
-//     const file = fs.createWriteStream(`${source.caseType}.csv`);
-//     const request = https.get(source.url, function (response) {
-//         response.pipe(file);
-//     });
-// })
-
-
-////////////////////  download CSV and convert to 2D array  ////////////////////
-// let fileConfirmed = fs.readFileSync("confirmed.csv", {encoding: 'utf8'});
-// let result = papa.parse(fileConfirmed, {
-//     complete: function(array2D) {
-//         console.log(array2D);
-//     }
-// })
-
-
-////////////////////  generate data.js  ////////////////////
-// let rawData = {};
-// let processedData = {};
-// function generateDatajs(array2D) {
-//     let rawData = {};
-//     rawData.time = array2D[0].slice(5);
-//     for (let row = 1; row < array2D.length; row++) {
-//         let series = {};
-//         series.province = array2D[row][0];
-//         series.country = array2D[row][1];
-//         series.confirmed = array2D[row].slice(5);
-//         rawData.case.push(series);
-//     }
-// }
-
-// .death?
-// .recovered?
+////////////////////  functions  ////////////////////
